464ac1f109e366fdc36c0374f7f43b9e049e5b6a
Brendan Phillips <Brendan Phillips>
2016-06-01 19:21:30 -0500

    Part one of the github lab

---
+++ app/src/main/java/ly/generalassemb/githublab/MainActivity.java
@@ -0,0 +1,21 @@
+package ly.generalassemb.githublab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+public class Car.java{
+
+        }
+public class Main.java{
+        public static void main
+
+        }
